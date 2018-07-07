38a8d354448965459d060aae0b6be5cf8af7fb69
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 13:18:25 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
     if person === "Freddy Mercury"
       data.each do |key, value|
         if key === :favorite_icecream_flavors
-          data.delete_if {value == "strawberry"}
+          data.delete_if {|x| x >= "strawberry"}
         end
       end
     end
