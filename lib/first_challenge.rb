62133b61064c17e6b89fce2c3ced6d8bb9a688fe
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 13:06:10 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
   contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
   end
-  puts contracts
+  puts contacts
   #remember to return your newly altered contacts hash!
   contacts
 end
